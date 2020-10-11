892fdbf7e643e610aaa04b148b599af8c6241ea9
Charles Zhang <charleszha2000@gmail.com>
2020-10-10 23:06:06 -0600

    basic server code

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/time', (req, res) => {
+    res.writeHead(200, {'ContentType' : 'text/plain'});
+    res.write(Date());
+    res.end();
+})
+
+app.listen(process.env.PORT);
