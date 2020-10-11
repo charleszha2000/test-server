9ba9848fe5f4420a784e62d41572c27d78b05c5e
Charles Zhang <charleszha2000@gmail.com>
2020-10-11 15:43:25 -0600

    mongodb on vm

--- server.js
+++ server.js
@@ -1,14 +1,52 @@
 const express = require('express');
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = "mongodb+srv://db-admin:vQvccC17ncVO79U0@cluster0.kt67l.azure.mongodb.net/CharlesZhang_M5?retryWrites=true&w=majority";
+
+
 app.get('/time', (req, res) => {
-    console.log("got request");
     res.writeHead(200, {'ContentType' : 'text/plain'});
     res.write(Date());
     res.end();
 })
 
-app.set('port', 80);
-app.listen(app.get('port'), function() {
-    console.log('Server up: http://localhost:' + app.get('port'));
+app.get('/quote', (req, res) => {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const db = client.db("CharlesZhang_M5");
+        db.collection("quotes").find({}).toArray(function(err, result) {
+            if (err) throw err;
+            let quote = result[getRandomInt(result.length)]; 
+            res.writeHead(200, {'ContentType' : 'text/plain'});
+            res.write(quote.quote);
+            res.write(" " + "-" + quote.name);
+            res.end();
+            client.close();
+        });
+    });
 });
+
+app.put('/quote', (req, res) => {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    req.query
+    client.connect(err => {
+        const db = client.db("CharlesZhang_M5");
+        db.collection("quotes").find({}).toArray(function(err, result) {
+            if (err) throw err;
+            let quote = result[getRandomInt(result.length)]; 
+            res.writeHead(200, {'ContentType' : 'text/plain'});
+            res.write(quote.quote);
+            res.write(" " + "-" + quote.name);
+            res.end();
+            client.close();
+        });
+    });
+});
+
+app.listen(process.env.PORT);
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
