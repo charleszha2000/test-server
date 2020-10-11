4f517d5a638af763962c9d46e9281fd6eca9c8aa
Charles Zhang <charleszha2000@gmail.com>
2020-10-10 23:21:14 -0600

    changed port to 8080

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/time', (req, res) => {
+    console.log("got request");
     res.writeHead(200, {'ContentType' : 'text/plain'});
     res.write(Date());
     res.end();
